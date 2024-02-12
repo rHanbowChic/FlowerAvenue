e0126f24b07b54ca67f7781e1a04f9b5e258fa59
Ect07 <ect07aha@hotmail.com>
2024-02-12 15:29:44 +0800

    Add project files.

---
+++ FlowerAvenue/FormWebview.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace FlowerAvenue
+{
+    partial class FormWebview
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormWebview));
+            this.webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView
+            // 
+            this.webView.AllowExternalDrop = true;
+            this.webView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.webView.CreationProperties = null;
+            this.webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView.Location = new System.Drawing.Point(0, 0);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(1200, 750);
+            this.webView.TabIndex = 0;
+            this.webView.ZoomFactor = 1D;
+            // 
+            // FormWebview
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1200, 750);
+            this.Controls.Add(this.webView);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormWebview";
+            this.Text = "The Flower Avenue";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FormWebview_FormClosing);
+            this.Load += new System.EventHandler(this.FormWebview_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+    }
+}
+
---
+++ FlowerAvenue/FormWebview.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlowerAvenue
+{
+    public partial class FormWebview : Form
+    {
+
+        string flveTempPath;
+        public FormWebview()
+        {
+            InitializeComponent();
+        }
+
+        public void deleteDir(string PATH)
+        {
+            System.IO.DirectoryInfo di = new DirectoryInfo(PATH);
+
+            foreach (FileInfo file in di.GetFiles())
+            {
+                file.Delete();
+            }
+            foreach (DirectoryInfo dir in di.GetDirectories())
+            {
+                dir.Delete(true);
+            }
+        }
+
+        private async void FormWebview_Load(object sender, EventArgs e)
+        {
+            string tempPath = Path.GetTempPath();
+            flveTempPath = Path.Combine(tempPath, "FloweraveResources");
+            string wv2TempPath = Path.Combine(tempPath, "FloweraveWebView2");
+            if (Directory.Exists(flveTempPath))
+                deleteDir(flveTempPath);
+            else if (File.Exists(flveTempPath))
+                File.Delete(flveTempPath);
+
+
+            CoreWebView2Environment corewv2Env = await CoreWebView2Environment.CreateAsync(userDataFolder: wv2TempPath);
+            await this.webView.EnsureCoreWebView2Async(corewv2Env);
+            
+            MemoryStream flveStream = new MemoryStream(Properties.Resources.flowerave_resources);
+            ZipArchive flveArchive = new ZipArchive(flveStream);
+            
+            flveArchive.ExtractToDirectory(flveTempPath);
+
+            this.webView.CoreWebView2.Settings.AreBrowserAcceleratorKeysEnabled = false;
+            this.webView.CoreWebView2.Settings.AreDefaultContextMenusEnabled = false;
+            this.webView.Source = new System.Uri(Path.Combine(flveTempPath, "index.html"), System.UriKind.Absolute);
+            
+        }
+
+
+        private void FormWebview_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (Directory.Exists(flveTempPath))
+                deleteDir(flveTempPath);
+        }
+    }
+}
